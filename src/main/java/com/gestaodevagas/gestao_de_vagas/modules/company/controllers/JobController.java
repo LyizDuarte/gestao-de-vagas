aafb77c92ea2659bdb0cf64b093bbf00b389f8f5
Luiz Duarte <looduarte21@gmail.com>
2024-12-28 14:41:16 -0300

    feat: job controller

---
+++ src/main/java/com/gestaodevagas/gestao_de_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,25 @@
+package com.gestaodevagas.gestao_de_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gestaodevagas.gestao_de_vagas.modules.company.entities.JobEntity;
+import com.gestaodevagas.gestao_de_vagas.modules.company.usecases.CreateJobUseCase;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+    @Autowired
+    private CreateJobUseCase createJobUseCase;
+
+    @PostMapping("/")
+    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+        return this.createJobUseCase.execute(jobEntity);
+    }
+}
