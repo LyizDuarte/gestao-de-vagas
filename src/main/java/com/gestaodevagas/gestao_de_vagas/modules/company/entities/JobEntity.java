4eead683d9a2ca23908b0964c0d47603a19624dd
Luiz Duarte <looduarte21@gmail.com>
2024-12-28 14:03:47 -0300

    feat: company controller

--- src/main/java/com/gestaodevagas/gestao_de_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/com/gestaodevagas/gestao_de_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,8 +26,11 @@ public class JobEntity {
     private String level;
 
     @ManyToOne
-    @JoinColumn(name = "company_id")
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    private UUID company_id;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
